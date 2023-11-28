9fbab2474a8c6edd79cff52eccf8b8425b103a58
Elhadji Sarr <elhadjindiayesarr20@gmail.com>
2023-11-28 16:55:28 -0500

    creation tp6

---
+++ tp6/TP6_inf1015/TP6_inf1015/TP6_inf1015.cpp
@@ -0,0 +1,10 @@
+#include "TP6_inf1015.h"
+
+TP6_inf1015::TP6_inf1015(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+TP6_inf1015::~TP6_inf1015()
+{}
---
+++ tp6/TP6_inf1015/TP6_inf1015/main.cpp
@@ -0,0 +1,10 @@
+#include "TP6_inf1015.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TP6_inf1015 w;
+    w.show();
+    return a.exec();
+}
